af6bdb853106c2696054fbe9c1920d6f1bf9c829
eandreoliveira <tqi_eandre@uolinc.com>
2022-11-09 18:29:08 -0300

    adicionar mensagem e arredondar valor do imc

--- src/main/kotlin/br/com/saude/imc/model/User.kt
+++ src/main/kotlin/br/com/saude/imc/model/User.kt
@@ -2,13 +2,14 @@ package br.com.saude.imc.model
 
 data class User(
     val imc: Double,
-){
+
+) {
     fun getmensagem() = when {
         imc < 18.5 -> "Magreza"
-        imc < 24.9 -> "Peso Normal"
-        imc < 29.9 -> "Sobrepeso"
-        imc < 34.9 -> "Obesidade grau I"
-        imc < 40 -> "Obesidade grau II"
+        imc < 25.0 -> "Peso Normal"
+        imc < 30.0 -> "Sobrepeso"
+        imc < 35.0 -> "Obesidade grau I"
+        imc < 40.0 -> "Obesidade grau II"
         else -> "Obesidade grau III"
     }
 }
--- src/main/kotlin/br/com/saude/imc/service/CalculateImcService.kt
+++ src/main/kotlin/br/com/saude/imc/service/CalculateImcService.kt
@@ -3,11 +3,15 @@ package br.com.saude.imc.service
 import br.com.saude.imc.dto.ImcForm
 import br.com.saude.imc.model.User
 import org.springframework.stereotype.Service
+import kotlin.math.roundToInt
 
 @Service
 class CalculateImcService{
     fun calculate(imcForm: ImcForm): User {
+
         val imcValue = imcForm.peso / (imcForm.altura * imcForm.altura)
-        return User(imc = imcValue)
+        val imcNearValue = (imcValue * 100.0).roundToInt() / 100.0
+
+        return User(imcNearValue)
     }
 }
