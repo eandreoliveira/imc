646ffe703f95f7c52ad2756dc67e5c6ef830736b
eandreoliveira <tqi_eandre@uolinc.com>
2023-02-15 18:18:48 -0300

    Adiciona metodo get e trata error not found

--- src/main/kotlin/br/com/saude/imc/controller/CalculateImcController.kt
+++ src/main/kotlin/br/com/saude/imc/controller/CalculateImcController.kt
@@ -5,6 +5,8 @@ import br.com.saude.imc.dto.ImcResponse
 import br.com.saude.imc.log.logger
 import br.com.saude.imc.service.CalculateImcService
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,6 +23,16 @@ class CalculateImcController (
     fun calculate(@RequestBody imcRequest: ImcRequest): ImcResponse {
         logger.info{ "Requisicao do Calculo de IMC" }
         return service.cadastrar(imcRequest)
+    }
+
+    @GetMapping
+    fun listar(): List<ImcResponse>{
+        return service.listar()
+    }
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): ImcResponse{
+        return service.buscarPorId(id)
     }
 
 }
--- src/main/kotlin/br/com/saude/imc/service/CalculateImcService.kt
+++ src/main/kotlin/br/com/saude/imc/service/CalculateImcService.kt
@@ -2,16 +2,19 @@ package br.com.saude.imc.service
 
 import br.com.saude.imc.dto.ImcRequest
 import br.com.saude.imc.dto.ImcResponse
+import br.com.saude.imc.exception.NotFoundException
 import br.com.saude.imc.log.logger
 import br.com.saude.imc.mapper.mapToImc
 import br.com.saude.imc.mapper.mapToImcResponse
 import br.com.saude.imc.repository.ImcRepository
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 import kotlin.math.roundToInt
 
 @Service
 class CalculateImcService(
-    private val repository: ImcRepository
+    private val repository: ImcRepository,
+    private val notFoundMessage: String = "Imc nao encontrado!",
 ){
     fun cadastrar(imcRequest: ImcRequest): ImcResponse {
         val imcNearValue = calculaImc(imcRequest)
@@ -27,4 +30,17 @@ class CalculateImcService(
         logger.info(imcNearValue.toString())
         return imcNearValue
     }
+
+    fun listar(): List<ImcResponse> {
+        return repository.findAll().stream().map {t ->
+            mapToImcResponse(t)
+        }.collect(Collectors.toList())
+    }
+
+    fun buscarPorId(id: Long): ImcResponse {
+        val imc = repository.findById(id)
+            .orElseThrow{ NotFoundException(notFoundMessage) }
+        return mapToImcResponse(imc)
+    }
+
 }
